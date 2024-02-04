e1e786552f9df9335f60e24e30c2a84a30b7fd32
jshim123 <126594113+jshim123@users.noreply.github.com>
2024-02-03 19:14:23 -0500

    Update babyblues.py

--- baby-blues/babyblues.py
+++ baby-blues/babyblues.py
@@ -2,17 +2,17 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import random
 
-# Set your Spotify API credentials
+# api key
 client_id = "6f3bc5b1ddd647558965125cde910218"
 client_secret = "b38fb25080cf45949a895fdbaec257b7"
 redirect_uri = "http://localhost:8888/callback"
 username = "justintshim"
 genre = 'joy'
 
-# Scopes define the level of access you need: creating and modifying user's playlist
+
 scope = "playlist-modify-public playlist-modify-private"
 
-# Setup Spotify auth
+# setup
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri=redirect_uri,
@@ -46,7 +46,7 @@ def get_playlist_id(playlist_name):
             playlists = None
     return None
 
-# Example usage
+# test
 playlist_id = get_playlist_id("joy Playlist")
 if playlist_id:
     print(f"Playlist ID: {playlist_id}")
@@ -69,23 +69,19 @@ def create_playlist_for_genre(genre_keyword):
     new_playlist = sp.user_playlist_create(user=username, name=playlist_name, public=True)
     new_playlist_id = new_playlist['id']
 
-    # Search for playlists that match the genre keyword
+    # Search
     playlists = sp.search(q=f"genre:{genre_keyword}", limit=10, type='playlist')
     playlist_ids = [playlist['id'] for playlist in playlists['playlists']['items']]
-
-    # Extract tracks from the found playlists
     track_ids = set()
     for pl_id in playlist_ids:
         tracks = sp.playlist_tracks(pl_id)
         track_ids.update([track['track']['id'] for track in tracks['items'] if track['track']])
 
-    # Convert track_ids set to a list for random sampling
     track_ids_list = list(track_ids)
 
-    # Select a subset of tracks
+
     selected_track_ids = random.sample(track_ids_list, min(20, len(track_ids_list)))
 
-    # Add new tracks to the playlist
     sp.playlist_add_items(new_playlist_id, selected_track_ids)
 
     print(f"New playlist created: {playlist_name}")
